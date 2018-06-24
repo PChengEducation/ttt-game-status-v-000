c0ffa89249a61fc7662c5e910b01278aefe0b7a9
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-24 04:19:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,9 +28,9 @@ end
 
 def full? (board)
 WIN_COMBINATIONS.each do |win_set|
-  if win_set.all? {|win_position| board[win_position] != "X"} || win_set.all? {|win_position| board[win_position] != "O"} && win_set.any? {|win_position| board[win_position] != " "}
+  if win_set.all? {|win_position| board[win_position] != "X"} && win_set.all? {|win_position| board[win_position] != "O"} && win_set.any? {|win_position| board[win_position] != " "}
     return true
-elsif win_set.any? {|win_position| board[win_position] == " "}
+else
 return false
 end
 end
